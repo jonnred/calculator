ba9757447a16b00a53f1a300eaaf49096577091f
jonnred <67404290+jonnred@users.noreply.github.com>
2020-07-12 11:55:57 +0800

    the css is not done yet
    I still have to learn how to align them in css then in js, I don't know what to do yet.

---
+++ java.js
@@ -0,0 +1,19 @@
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("btn2");
+var btn3 = document.querySelector("btn3");
+var btn4 = document.querySelector("btn4");
+var btn5 = document.querySelector("btn5");
+var btn6 = document.querySelector("btn6");
+var btn7 = document.querySelector("btn7");
+var btn8 = document.querySelector("btn8");
+var btn9 = document.querySelector("btn9");
+var btn0 = document.querySelector("btn0");
+var x;
+var y;
+var quant;
+
+btn0.addEventListener("click", myfunc())
+ 
+function myfunc{
+    alert("heyy");
+}
