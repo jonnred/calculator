f4329c893e8cbdd6f2c9de36d3eee748bbf3115d
jonn red <jonnred1123@gmail.com>
2021-03-27 07:40:36 +0800

    almost complete

--- index.js
+++ index.js
@@ -1,14 +1,87 @@
-/****** number 1 to 10 **********/
-    let firstNumber = ""; 
-    let secondNumber = "";
-    const numbers = document.querySelectorAll(".digit");
-    for (let index = 0; index < numbers.length; index++) {
-        const element = numbers[index];
-        element.addEventListener("click" , (e) => {
-         firstNumber = firstNumber.concat(e.target.innerText);
-         console.log(firstNumber);
-    })        
+/****** number 1 to 10 and some plus**********/
+    var firstNumber = ""; 
+    var secondNumber = "";
+    const numbersFunc = (num) =>{
+        const numbers = document.querySelectorAll(".num");
+        for (let index = 0; index < numbers.length; index++) {
+            const element = numbers[index];
+            element.addEventListener("click" , (e) => {
+                firstNumber = firstNumber.concat(e.target.innerText);
+                console.log(firstNumber)           
+            })
+        }
+            operatorFunc();    
+
     }
+        // OPERATOR PLUS MINUS DIVIDE MULTIPLY
+    const operatorFunc = () => {
+
+        const operator = document.querySelectorAll(".operator");
+            for (let index = 0; index < operator.length; index++) {
+                operator[index].addEventListener("click" , function(){
+                    if (this.id=="minus"){
+                    console.log('Subtraction');
+                    }
+                    else if (this.id=="result"){
+                    console.log('Result');
+                    }
+                    else if (this.id=="ac"){
+                    console.log('AC');
+                    }
+                    else if (this.id=="del"){
+                    console.log('Delete');
+                    }
+                    else if (this.id=="dot"){
+                    console.log('dot');
+                    }
+                    firstNumber = secondNumber;
+                    console.log("firstN = " +firstNumber)
+                    console.log(secondNumber)
+                })
+                            console.log(' im working');
+        }
+    }
+
+        //  EQUALS BUTTON
+        // const result = document.querySelector("#result");
+    numbersFunc();
+
+    // const fNumbers = document.querySelectorAll(".first-output")[0];
+    // let firstNum = fNumbers.innerHTML;
+    // console.log(firstNum);
+
+    // const sNumbers = document.querySelectorAll(".second-output")[0];
+    // let secondNum = sNumbers.innerHTML;
+    // console.log(secondNum);
+
+    // let answer = firstNum + secondNum;
+    // answer = eval(answer)
+
+    // console.log(answer)
+
+
+
+
+
+// var a = '100';
+// var b = '10';
+// var o = "+";
+// var at = parseInt(a);
+// var bt = parseInt(b);
+    
+// var c = at +o + bt;
+
+//     console.log("var C = "+ c);
+
+//     console.log(a);
+//     console.log(b);
+    
+//     console.log(at);
+//     console.log(bt);
+    
+//     console.log(parseInt(a+b));
+//     console.log(parseInt(a+b));
+
     
 /* 
 1.click number
